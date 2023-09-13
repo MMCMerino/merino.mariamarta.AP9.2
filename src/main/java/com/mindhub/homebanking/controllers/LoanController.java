d4e53177d86aeadf60f62fa3945ec7726400f16d
MMCMerino <mariamartamerino@gmail.com>
2023-09-13 12:46:30 -0300

    Task11 test

--- src/main/java/com/mindhub/homebanking/controllers/LoanController.java
+++ src/main/java/com/mindhub/homebanking/controllers/LoanController.java
@@ -45,7 +45,7 @@ public class LoanController {
 
     @GetMapping("/loans/{id}")
     public ResponseEntity<Object> getLoanByName(@PathVariable String name,
-                                              Authentication authentication) {
+                                                Authentication authentication) {
         Client client = clientService.findByEmail(authentication.getName());
         Loan loan = loanService.findByName(name);
 
@@ -74,50 +74,48 @@ public class LoanController {
         Loan loan = loanService.findById(loanApplicationDTO.getLoanId());
 
 
+        if (loanApplicationDTO.getPayments() == 0 || loanApplicationDTO.getToAccountNumber().isBlank() || loanApplicationDTO.getAmount() <= 10000) {
+            return new ResponseEntity<>("Invalid parameters", HttpStatus.FORBIDDEN);
 
+        } else if (loan.equals(loanApplicationDTO.getLoanId())) {
+            return new ResponseEntity<>("The requested loan does not exist", HttpStatus.FORBIDDEN);
 
+             } else if (loanApplicationDTO.getAmount() > loan.getMaxAmount()) {
+                    return new ResponseEntity<>("Maximum amount exceeded", HttpStatus.FORBIDDEN);
+                 } else if (!loan.getPayments().contains(loanApplicationDTO.getPayments())) {
+                        return new ResponseEntity<>("Choose another financing plan", HttpStatus.FORBIDDEN);
 
-       if(loanApplicationDTO.getPayments() == 0 ||loanApplicationDTO.getToAccountNumber().isBlank() ||loanApplicationDTO.getAmount()<=10000){
-            return new ResponseEntity<>("Invalid parameters",HttpStatus.FORBIDDEN);
+                     } else if (!(client.getAccounts().contains(toAccount))) {
+                             return new ResponseEntity<>("Account error", HttpStatus.FORBIDDEN);
+                            }/*else if(!client.getClientLoans().stream().
+                                   filter(clientLoans ->clientLoans.getLoan().equals(loanApplicationDTO.getLoanId()))
+                                   .collect(Collectors.toSet()).isEmpty()){//todo-> verificar que el prestamo no este tomado
+                               return new ResponseEntity<>("You already acquired that loan",HttpStatus.FORBIDDEN);
+                        }*/ else {
+                            //Creo el nuevo prestamo
 
-        } else if(loan.equals(loanApplicationDTO.getLoanId())){
-            return new ResponseEntity<>("The requested loan does not exist",HttpStatus.FORBIDDEN);
+                            ClientLoan clientLoanNew = new ClientLoan(loanApplicationDTO.getPayments(),
+                                    (loanApplicationDTO.getAmount() + loanApplicationDTO.getAmount() * 0.2));
+                            client.addClientLoan(clientLoanNew);
+                            loan.addClientLoanLoan(clientLoanNew);
+                            clientLoanService.save(clientLoanNew);
 
-        }else if(loanApplicationDTO.getAmount() > loan.getMaxAmount() ){
-            return new ResponseEntity<>("Maximum amount exceeded",HttpStatus.FORBIDDEN);
-        }else if(! loan.getPayments().contains(loanApplicationDTO.getPayments())){
-            return new ResponseEntity<>("Choose another financing plan",HttpStatus.FORBIDDEN);
+                            //Agrego la transaccion y modifico los balances
 
-        }else if (!(client.getAccounts().contains(toAccount))) {
-           return new ResponseEntity<>("Account error", HttpStatus.FORBIDDEN);
-       }/*else if(!client.getClientLoans().stream().
-               filter(clientLoans ->clientLoans.getLoan().equals(loanApplicationDTO.getLoanId()))
-               .collect(Collectors.toSet()).isEmpty()){//todo-> verificar que el prestamo no este tomado
-           return new ResponseEntity<>("You already acquired that loan",HttpStatus.FORBIDDEN);
-        }*/else{
-            //Creo el nuevo prestamo
+                            Transaction transactionNew = new Transaction(TransactionType.CREDIT,
+                                    loanApplicationDTO.getAmount(),
+                                    "Obtaining a loan ", LocalDate.now());
+                            toAccount.addTransaction(transactionNew);
+                            toAccount.setBalance(toAccount.getBalance() + loanApplicationDTO.getAmount());// todo -> ver donde va el +20%
+                            transactionService.save(transactionNew);
+                            accountService.save(toAccount);
 
-            ClientLoan clientLoanNew = new ClientLoan(loanApplicationDTO.getPayments(),(loanApplicationDTO.getAmount()+loanApplicationDTO.getAmount()*0.2));
-            client.addClientLoan(clientLoanNew);
-            loan.addClientLoanLoan(clientLoanNew);
-            clientLoanService.save(clientLoanNew);
-
-            //Agrego la transaccion y modifico los balances
-
-             Transaction transactionNew = new Transaction(TransactionType.CREDIT,
-                     loanApplicationDTO.getAmount(),"Obtaining a loan ",LocalDate.now());
-            toAccount.addTransaction(transactionNew);
-            toAccount.setBalance(toAccount.getBalance()+loanApplicationDTO.getAmount());// todo -> ver donde va el +20%
-            transactionService.save(transactionNew);
-            accountService.save(toAccount);
-
-            return new ResponseEntity<>("Exit",HttpStatus.CREATED);
-
-        }
-
+                            return new ResponseEntity<>("Exit", HttpStatus.CREATED);
+                     }
 
 
     }
 
-
 }
+
+
